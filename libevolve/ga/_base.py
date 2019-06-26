3920aa07797c1ce091119ed798b929bdd38d868b
Sameh Mohamed <sameh.kamaleldin@gmail.com>
2019-06-26 03:16:50 +0100

    update generic ga class

--- libevolve/ga/_base.py
+++ libevolve/ga/_base.py
@@ -1,7 +1,9 @@
 # -*- coding: utf-8 -*-
 
 from abc import abstractmethod
-from .util import GeneticHistory
+from random import Random
+from .util import GeneticHistory, normalise
+from ..common import *
 
 
 class GeneticAlgorithm:
@@ -12,7 +14,10 @@ class GeneticAlgorithm:
                  nb_generations=20,
                  mutation_size=1,
                  mutation_probability=0.3,
-                 crossover_probability=0.5):
+                 crossover_probability=0.5,
+                 selection_size=20,
+                 seed=1234,
+                 verbose=0):
         """ Initialise a new instance of the `GeneticAlgorithm` class
 
         Parameters
@@ -27,6 +32,12 @@ class GeneticAlgorithm:
             the probability of mutation for the chosen genes
         crossover_probability : float
             probability of crossover
+        selection_size : int
+            the size of natural selection group
+        seed : int
+            random seed
+        verbose : int
+            verbosity level
 
         Examples
         ----------
@@ -40,36 +51,69 @@ class GeneticAlgorithm:
         self.mutation_probability = mutation_probability
         self.crossover_probability = crossover_probability
 
+        self.verbose = verbose
+        self.seed = seed
+        self.rs = Random(seed)
         self.parameters = None
         self.fitness_function = None
         self.objective_weights = None
         self.history = GeneticHistory()
 
-    def _mutate(self, individual):
-        """ Mutate a single individual
+    @abstractmethod
+    def _crossover(self, parent1, parent2):
+        """ Crossover between two parents individuals
 
         Parameters
         ----------
-        individual : list
-            the individual list of `EvoParam`
+        parent1 : Individual
+            first parent individual
+        parent2 : Individual
+            second parent individual
+
+        Returns
+        -------
+        Individual
+            first child individual
+        Individual
+            second child individual
+        """
+        pass
+
+    @abstractmethod
+    def natural_selection(self, population, fitness_scores, *args, **kwargs):
+        """ Perform genetic natural selection
+
+        population : list
+            list of individuals
+        fitness_fitness_scores : list
+            list of fitness scores lists
+        args : list
+            other un named arguments
+        kwargs : dict
+            other named arguments
 
         Returns
         -------
         list
-            the mutated individual list of `EvoParam`
+            list of chosen individuals
         """
+        pass
 
-    def evolve(self, parameters, fitness_function, objective_weights):
+    def evolve(self, parameters, fitness_function, objective_weights, *args, **kwargs):
         """ Perform evolution on the specified parameters and objective function
 
-        parameters : Iterable
+        parameters : list
             the set of evolutionary learning parameters
         fitness_function : function
             the fitness function. Expects named parameters that are equal or subset of the input parameters with the
             same names as specified in the input parameters. Must return an iterable.
-        objective_weights : Iterable
+        objective_weights : list
             the assigned weights to the fitness function output objective values. Positive values denote maximisation
             objective while negative values represent minimisation objective of the corresponding objective output.
+        args : list
+            other un named arguments
+        kwargs : dict
+            other named arguments
 
         Returns
         -------
@@ -87,6 +131,31 @@ class GeneticAlgorithm:
         >>> ga = GeneticAlgorithm(population_size=10, nb_generations=15, mutation_size=1, mutation_probability=0.9)
         >>> best_solution, best_score, history = ga.evolve()
         """
-        best_solution, score = None, 0.0
+        best_solution, best_score = None, 0.0
 
-        return best_solution, score
+        nb_params = len(parameters)
+        nb_objectives = len(objective_weights)
+        param_names = [p.name for p in parameters]
+        population = []
+
+        # generate initial population
+        for _ in range(self.population_size):
+            ind = Individual(parameters, seed=self.rs.randint(0, 9999999999))
+            population.append(ind)
+
+        current_generation = population
+        current_generation_fitness = [[] for _ in range(nb_objectives)]
+
+        for generation_idx in range(self.nb_generations):
+            for ind in current_generation:
+                ind_fitness = fitness_function(**ind.key_params)
+                for idx, fitness_value in enumerate(ind_fitness):
+                    current_generation_fitness[idx].append(fitness_value)
+
+                # selection
+
+                # crossover
+
+                # mutations
+
+        return best_solution, best_score, self.history
